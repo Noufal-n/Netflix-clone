3344cd4f535411f0208c09613c32b40eb1fc03bc
Muhammed Noufal N <muhammednoufaln369@gmail.com>
2023-04-30 16:15:12 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from "react"
 
 function App() {
+  let [state, setState] = useState(false)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1 onClick={() => setState(!state)} >click here</h1>
+      {
+        state ? <h3 style={{color:'white'}} >you can see me</h3> : <h3>i am gone</h3>
+      }
+
     </div>
   );
 }
